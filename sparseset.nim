7e6b42b23fd0a01c1c88afa4f02d4516b7b2dd85
planetis-m <b3lievem@yandex.com>
2020-12-11 17:42:38 +0200

    try better api

--- sparseset.nim
+++ sparseset.nim
@@ -5,36 +5,36 @@ type
   Entry*[K, V] = tuple
     key: K
     value: V
-  SparseSet*[K: SomeNumber, V] = object
+  SparseSet*[K, V] = object ## K must be an Integer type
     len: int
     sparse: seq[K]          ## Mapping from sparse handles to dense values.
     dense: seq[Entry[K, V]] ## Mapping from dense values to sparse handles.
 
-template empty: untyped = K(s.sparse.len) # high(K) instead?
+template empty*[K](t: typedesc[K]): untyped = K(s.sparse.len)
 
 proc initSparseSet*[K, V](cap: Natural): SparseSet[K, V] =
   result = SparseSet[K, V](dense: newSeq[Entry[K, V]](cap), sparse: newSeq[K](cap))
-  result.sparse.fill(K(cap))
+  result.sparse.fill(when compiles(empty(K)): empty(K) else: K(cap))
 
 proc contains*[K, V](s: SparseSet[K, V], key: K): bool =
   ## Returns true if the sparse is registered to a dense index.
-  int(key) < s.sparse.len and s.sparse[key] != empty
+  key.int < s.sparse.len and s.sparse[key.int] != empty(K)
 
 proc `[]=`*[K, V](s: var SparseSet[K, V], key: K, value: sink V) =
   ## Inserts a `(key, value)` pair into `s`.
   assert int(key) < s.sparse.len, "key must be under len of SparseSet"
-  var denseIndex = s.sparse[key]
-  if denseIndex == empty:
+  var denseIndex = s.sparse[key.int]
+  if denseIndex == empty(K):
     denseIndex = K(s.len)
-    s.dense[denseIndex].key = key
-    s.sparse[key] = denseIndex
+    s.dense[denseIndex.int].key = key
+    s.sparse[key.int] = denseIndex
     inc(s.len)
-  s.dense[denseIndex].value = value
+  s.dense[denseIndex.int].value = value
 
 template get(s, key) =
   if key notin s:
     raise newException(KeyError, "key not in SparseSet")
-  result = s.dense[s.sparse[key]].value
+  result = s.dense[s.sparse[key.int].int].value
 
 proc `[]`*[K, V](s: var SparseSet[K, V], key: K): var V =
   ## Retrieves the value at `s[key]`. The value can be modified.
@@ -46,37 +46,37 @@ proc `[]`*[K, V](s: SparseSet[K, V], key: K): lent V =
   ## If `key` is not in `s`, the `KeyError` exception is raised.
   get(s, key)
 
-proc delete*[K, V](s: var SparseSet[K, V], key: K) =
+proc del*[K, V](s: var SparseSet[K, V], key: K) =
   ## Deletes `key` from sparse set `s`. Does nothing if the key does not exist.
-  let denseIndex = s.sparse[key]
-  if denseIndex != empty:
+  let denseIndex = s.sparse[key.int]
+  if denseIndex != empty(K):
     let lastIndex = s.len - 1
     let lastKey = s.dense[lastIndex].key
-    s.sparse[lastKey] = denseIndex
-    s.sparse[key] = empty
-    s.dense[denseIndex] = move(s.dense[lastIndex])
+    s.sparse[lastKey.int] = denseIndex
+    s.sparse[key.int] = empty(K)
+    s.dense[denseIndex.int] = move(s.dense[lastIndex])
     s.len.dec
 
 proc sort*[K, V](s: var SparseSet[K, V], cmp: proc (x, y: V): int, order = SortOrder.Ascending) =
   for i in 1 ..< s.len:
     let x = move(s.dense[i].value)
     let xKey = s.dense[i].key
-    let xIndex = s.sparse[xKey]
+    let xIndex = s.sparse[xKey.int]
     var j = i - 1
     while j >= 0 and cmp(x, s.dense[j].value) * order < 0:
       let jKey = s.dense[j].key
-      s.sparse[s.dense[j + 1].key] = s.sparse[jKey]
+      s.sparse[s.dense[j + 1].key.int] = s.sparse[jKey.int]
       s.dense[j + 1].key = jKey
       s.dense[j + 1].value = move(s.dense[j].value)
       dec(j)
-    s.sparse[s.dense[j + 1].key] = xIndex
+    s.sparse[s.dense[j + 1].key.int] = xIndex
     s.dense[j + 1].key = xKey
     s.dense[j + 1].value = x
 
 proc clear*[K, V](s: var SparseSet[K, V]) =
-  s.sparse.fill(empty)
+  s.sparse.fill(empty(K))
   when not supportsCopyMem(V):
-    for i in 0..<s.len: s.dense[i].value = default(V)
+    for i in 0 ..< s.len: s.dense[i].value = default(V)
   s.len = 0
 
 iterator keys*[K, V](s: SparseSet[K, V]): K =
@@ -92,29 +92,3 @@ iterator pairs*[K, V](s: SparseSet[K, V]): Entry[K, V] =
     yield s.dense[i]
 
 proc len*[K, V](s: SparseSet[K, V]): int = s.len
-
-when isMainModule:
-  var x = initSparseSet[uint16, int](128)
-  assert x.len == 0
-  let ent1 = 1'u16
-  let ent2 = 2'u16
-  x[ent1] = 5
-  x[ent2] = 4
-  assert(x.len == 2)
-  assert x[ent1] == 5
-  assert x[ent2] == 4
-  x.sort(cmp)
-  assert x[ent1] == 5
-  assert x[ent2] == 4
-  x.delete(ent1)
-  assert x.len == 1
-  x.clear()
-  assert x.len == 0
-  assert ent2 notin x
-  x[ent1] = 10
-  assert x.len == 1
-  assert x[ent1] == 10
-  x.delete(ent1)
-  assert x.len == 0
-  x[ent2] = 9
-  assert x[ent2] == 9
---
+++ tests/tsparseset.nim
@@ -0,0 +1,32 @@
+import sparseset
+
+type Entity = distinct uint16
+proc `==`(a, b: Entity): bool {.borrow.}
+proc `$`(x: Entity): string {.borrow.}
+template empty*(t: typedesc[Entity]): Entity = Entity(0) #should break
+
+block:
+  var x = initSparseSet[Entity, int](128)
+  assert x.len == 0
+  let ent1 = 1.Entity
+  let ent2 = 2.Entity
+  x[ent1] = 5
+  x[ent2] = 4
+  assert(x.len == 2)
+  assert x[ent1] == 5
+  assert x[ent2] == 4
+  x.sort(cmp)
+  assert x[ent1] == 5
+  assert x[ent2] == 4
+  x.del(ent1)
+  assert x.len == 1
+  x.clear()
+  assert x.len == 0
+  assert ent2 notin x
+  x[ent1] = 10
+  assert x.len == 1
+  assert x[ent1] == 10
+  x.del(ent1)
+  assert x.len == 0
+  x[ent2] = 9
+  assert x[ent2] == 9
